78f5f016964b2f5924d926b97cf58e56e7fcd1cb
Ray Lopez <raylopez509@gmail.com>
2025-02-26 13:54:54 -0800

    added mergesort function Mergesort function recursively sorts an array

--- mergesort.js
+++ mergesort.js
@@ -1,13 +1,10 @@
-let array = [2, 1, 3, 4];
-
 //if array == length 1, done
 //else
 //sort left half
 //sort right half
 //merge
 
-arr1 = [3, 4, 7];
-arr2 = [1, 2, 6];
+array = [105, 79, 100, 110];
 
 function merge(arr1, arr2) {
   let sortedArr = [];
@@ -17,15 +14,25 @@ function merge(arr1, arr2) {
     if(arr1[i] <= arr2[j] || j === arr2.length) {
       sortedArr.push(arr1[i]);
       i++;
-      console.log(`i: ${i}`);
     }
     else if (arr1[i] > arr2[j] || i === arr1.length) {
       sortedArr.push(arr2[j]);
       j++;
-      console.log(`j: ${j}`);
     }
   }
   return sortedArr;
 }
 
-console.log(merge(arr1, arr2));
+function mergeSort(array) {
+  if(array.length === 1) {
+    return array;
+  }
+  else {
+    let splitIndex = Math.floor(array.length / 2);
+    let splitArrayLeft = array.slice(0, splitIndex);
+    let splitArrayRight = array.slice(splitIndex, array.length);
+    return merge(mergeSort(splitArrayLeft), mergeSort(splitArrayRight));
+  }
+}
+
+console.log(mergeSort(array));
